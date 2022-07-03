acde0c7b17ae42e1d691c896c75c59f507a3a7a0
simonsajji <simonmathai777@gmail.com>
2022-07-03 11:18:57 +0530

    Updates

---
+++ precache-manifest.361766da96ae0e568b0b53bc6c36036a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c76605244504073fc321eecef8f04a",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "6d732f93acebfde71af9",
+    "url": "/react-tic-tac-toe/static/css/main.2d8e97df.chunk.css"
+  },
+  {
+    "revision": "856bd64a21dbd1a70913",
+    "url": "/react-tic-tac-toe/static/js/2.39a0290e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.39a0290e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d732f93acebfde71af9",
+    "url": "/react-tic-tac-toe/static/js/main.81cba290.chunk.js"
+  },
+  {
+    "revision": "d9255e6de55285e18dc3",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.81654cbd.js"
+  }
+]);
