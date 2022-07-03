41688d1c267735a5390a315b37d7eb08151cac9c
simonsajji <simonmathai777@gmail.com>
2022-07-03 11:19:38 +0530

    react -tic-tac-toe

---
+++ src/modules/Result.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Result(props) {
+    return (
+        <div className='modal-result'>
+            The {props.winner} is the winner        
+        </div>
+    );
+}
+
+export default Result;
